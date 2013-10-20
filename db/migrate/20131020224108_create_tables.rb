cf6f5acdab5030ae62fd4aab55180778344c1dd5
DrRobotmck <mckenneth.scott@icloud.com>
2013-10-20 19:10:40 -0400

    added form, new, and show pages.

---
+++ db/migrate/20131020224108_create_tables.rb
@@ -0,0 +1,10 @@
+class CreateTables < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name, null: false
+      t.string :email, null: false
+      t.timestamps
+      t.string :password_digest, null: false
+    end
+  end
+end
